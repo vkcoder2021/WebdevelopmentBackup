f8fc1f5065989a17c3dd05b1871ac896e0987f74
vikashkumar <vkindia2022@gmail.com>
2023-11-22 16:20:49 +0530

    added Node.js 2.1 ,2.2,2.3

---
+++ 2.1UsingNode/index.js
@@ -0,0 +1,3 @@
+console.log("Hii there i am using node ")
+var a=10;
+console.log (a);
---
+++ 2.2NativeModules/solution.js
@@ -0,0 +1,14 @@
+const fs = require("fs");
+
+fs.writeFile("message.txt", "Hello Node", (err) => {
+  if (err) throw err;
+  console.log("The file has been saved!");
+});
+
+fs.readFile("message.txt", "utf8", (err, data) => {
+  if (err) throw err;
+  console.log(data);
+});
+
+// "https://nodejs.org/dist/latest-v20.x/docs/api/ " 
+// Link for all your native node modules 

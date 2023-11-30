83494dccd19c2e0733b454643469b8f39ff1ab6a
vikashkumar <vkindia2022@gmail.com>
2023-11-30 17:59:32 +0530

    adde

--- 3.4Middleware/solution4.js
+++ 3.4Middleware/solution4.js
@@ -10,9 +10,10 @@ const port = 3000;
 var bandName = "";
 
 app.use(bodyParser.urlencoded({ extended: true }));
-
+var a=10;
 function bandNameGenerator(req, res, next) {
   console.log(req.body);
+   a=11;
   bandName = req.body["street"] + req.body["pet"];
   next();
 }
@@ -20,11 +21,15 @@ function bandNameGenerator(req, res, next) {
 app.use(bandNameGenerator);
 
 app.get("/", (req, res) => {
+  console.log("OK");
+  console.log(a);
   res.sendFile(__dirname + "/public/index.html");
 });
 
 app.post("/submit", (req, res) => {
   res.send(`<h1>Your band name is:</h1><h2>${bandName}✌️</h2>`);
+  a=12;
+  console.log(a);
 });
 
 app.listen(port, () => {
---
+++ sci-fi-card-hover-effect/README.md
@@ -0,0 +1,4 @@
+# Sci fi card hover effect
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/Smit29/pen/BavyNNp](https://codepen.io/Smit29/pen/BavyNNp).
+
